56d506639df72be1da0c4dc7b002b89ff3872b6e
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 20:37:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-sunil7756"
+    key    = "terraform-container/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
 module "vpc" {
   source = "./modules/vpc"
 }
@@ -14,8 +20,8 @@ module "security_groups" {
 
 module "ec2_instances" {
   source = "./modules/ec2_instances"
-  web_ami_id = "ami-xxxxxx"  # Replace with your actual AMI ID for Web tier
-  app_ami_id = "ami-xxxxxx"  # Replace with your actual AMI ID for App tier
+  web_ami_id = "ami-00bb6a80f01f03502"  # Replace with your actual AMI ID for Web tier
+  app_ami_id = "ami-00bb6a80f01f03502"  # Replace with your actual AMI ID for App tier
 }
 
 module "rds" {
