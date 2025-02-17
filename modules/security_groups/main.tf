27357a2753256d631c94c6cf93d56b54ef082815
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 20:30:55 +0530

    Create main.tf

---
+++ modules/security_groups/main.tf
@@ -0,0 +1,65 @@
+# modules/security_groups/main.tf
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_security_group"
+  description = "Allow inbound HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "app_security_group"
+  description = "Allow inbound traffic from web servers"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"] # web subnet CIDR block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db_security_group"
+  description = "Allow inbound traffic from app servers"
+
+  ingress {
+    from_port   = 1521  # Default Oracle port
+    to_port     = 1521
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.2.0/24"] # app subnet CIDR block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
